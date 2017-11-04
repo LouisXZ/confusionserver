4785a89ddf46e5934141cfc48cb4b93ada1babf0
LouisXZ <louiszhouxh@gmail.com>
2017-11-04 16:58:00 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '143476713048470',
+        clientSecret: '6af834e479981a2f8130bd5bd7e5c1c4'
+    }
 }
